d5096846c2f0baa53bc77a0217e092c9e73b81d9
MIKE MIKE <mikeo6000@hotmail.com>
2022-01-23 13:34:07 -0500

    P2

---
+++ PurrcationAPI/PurrcationAPI.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
+
+namespace PurrcationAPI.Test
+{
+  
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
