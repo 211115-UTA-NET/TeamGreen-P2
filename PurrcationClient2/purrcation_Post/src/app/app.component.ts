9bdacc246b80f4a09f47b07b97edc7a4ae6798eb
MIKE MIKE <mikeo6000@hotmail.com>
2022-01-28 09:50:25 -0500

    P2

---
+++ PurrcationClient2/purrcation_Post/src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent  {
+  title = 'purrcation';
+  data2: any = {};
+  public data:any = []
+
+  constructor(private http: HttpClient) {}
+  
+  
+  postData(info: string){
+    
+    const url ='https://localhost:7112/signup/?user='+info;
+    this.http.get(url).subscribe((res)=>{
+      this.http.jsonp
+      this.data = res
+      console.log(this.data)
+
+      // console.log(this.getData.arguments.
+    })
+  }
+
+ // ngOnInit(): void {
+ //   this.postData(name: string)
+  //}
+
+ }
